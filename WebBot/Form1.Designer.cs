33644a60822d13d59f547d4596243030034b2f2c
mustafa sarı <kemal_sari_267@hotmail.com>
2019-09-18 22:55:54 +0300

    v1

--- WebBot/Form1.Designer.cs
+++ WebBot/Form1.Designer.cs
@@ -28,13 +28,214 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBoxUrl = new System.Windows.Forms.TextBox();
+            this.btnGetData = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelUrunAdi = new System.Windows.Forms.Label();
+            this.labelAltBaslik = new System.Windows.Forms.Label();
+            this.labelFiyat = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelKirilim = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Url :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtBoxUrl
+            // 
+            this.txtBoxUrl.Location = new System.Drawing.Point(164, 3);
+            this.txtBoxUrl.Name = "txtBoxUrl";
+            this.txtBoxUrl.Size = new System.Drawing.Size(538, 20);
+            this.txtBoxUrl.TabIndex = 1;
+            this.txtBoxUrl.Text = "https://urun.n11.com/outdoor-kameralar/eken-h9r-4k-ultra-hd-wifi-aksiyon-kamerasi" +
+    "-yedek-batarya-P359363105";
+            // 
+            // btnGetData
+            // 
+            this.btnGetData.Location = new System.Drawing.Point(51, 6);
+            this.btnGetData.Name = "btnGetData";
+            this.btnGetData.Size = new System.Drawing.Size(75, 23);
+            this.btnGetData.TabIndex = 2;
+            this.btnGetData.Text = "Veri Çek";
+            this.btnGetData.UseVisualStyleBackColor = true;
+            this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ürün Adı :";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Alt Başlık :";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Fiyat :";
+            this.label4.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelUrunAdi
+            // 
+            this.labelUrunAdi.AutoSize = true;
+            this.labelUrunAdi.Location = new System.Drawing.Point(85, 65);
+            this.labelUrunAdi.Name = "labelUrunAdi";
+            this.labelUrunAdi.Size = new System.Drawing.Size(0, 13);
+            this.labelUrunAdi.TabIndex = 0;
+            this.labelUrunAdi.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelAltBaslik
+            // 
+            this.labelAltBaslik.AutoSize = true;
+            this.labelAltBaslik.Location = new System.Drawing.Point(85, 88);
+            this.labelAltBaslik.Name = "labelAltBaslik";
+            this.labelAltBaslik.Size = new System.Drawing.Size(0, 13);
+            this.labelAltBaslik.TabIndex = 0;
+            this.labelAltBaslik.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelFiyat
+            // 
+            this.labelFiyat.AutoSize = true;
+            this.labelFiyat.Location = new System.Drawing.Point(85, 113);
+            this.labelFiyat.Name = "labelFiyat";
+            this.labelFiyat.Size = new System.Drawing.Size(0, 13);
+            this.labelFiyat.TabIndex = 0;
+            this.labelFiyat.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(88, 143);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(296, 265);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Açıklama :";
+            this.label5.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(400, 143);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(302, 265);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 39);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Ürün Kırılım :";
+            this.label7.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelKirilim
+            // 
+            this.labelKirilim.AutoSize = true;
+            this.labelKirilim.Location = new System.Drawing.Point(96, 39);
+            this.labelKirilim.Name = "labelKirilim";
+            this.labelKirilim.Size = new System.Drawing.Size(0, 13);
+            this.labelKirilim.TabIndex = 0;
+            this.labelKirilim.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(467, 127);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(175, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Resimlere Tıklayarak büyütebilirsiniz";
+            this.label6.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btnGetData);
+            this.Controls.Add(this.txtBoxUrl);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelFiyat);
+            this.Controls.Add(this.labelAltBaslik);
+            this.Controls.Add(this.labelUrunAdi);
+            this.Controls.Add(this.labelKirilim);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxUrl;
+        private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelUrunAdi;
+        private System.Windows.Forms.Label labelAltBaslik;
+        private System.Windows.Forms.Label labelFiyat;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelKirilim;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- WebBot/Form1.cs
+++ WebBot/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,78 @@ namespace WebBot
 {
     public partial class Form1 : Form
     {
+        HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+        WebClient client = new WebClient();
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGetData_Click(object sender, EventArgs e)
+        {
+            tableLayoutPanel1.Controls.Clear();
+            client.Encoding = Encoding.UTF8;
+            client.d
+            string html = client.DownloadString(txtBoxUrl.Text);
+            document.LoadHtml(html);
+            //*[@id="contentProDetail"]/div/div[3]/div[2]/div[1]/div/h2
+            string urunAd = document.DocumentNode.SelectSingleNode("//*[@id=\"contentProDetail\"]/div/div[3]/div[2]/div[1]/div/h1").InnerText.Trim();
+            string urunAltBaslık = document.DocumentNode.SelectSingleNode("//*[@id=\"contentProDetail\"]/div/div[3]/div[2]/div[1]/div/h2").InnerText.Trim();
+            var aciklama= document.DocumentNode.SelectNodes("//*[@id=\"tabPanelProDetail\"]/div/section[3]/div");
+            string urunFiyat = document.DocumentNode.SelectSingleNode("//*[@id=\"contentProDetail\"]/div/div[3]/div[2]/div[3]/div[2]/div/div[1]/div/ins").InnerText.Trim();
+            var resimler = document.DocumentNode.SelectNodes("//*[@id=\"contentProDetail\"]/div/div[3]/div[1]/figure/div[2]/ul/li");
+            var kategoriKirilim = document.DocumentNode.SelectNodes("//*[@id=\"breadCrumb\"]/ul/li");
+            var varyantlar = document.DocumentNode.SelectNodes("//*[@id=\"autoRecBox\"]/div/div");
+            //*[@id="adSlider"]/div
+            string kirilim = "";
+            foreach (var item in kategoriKirilim)
+            {
+                kirilim += item.InnerText.Trim().Replace("amp;", "&")+">";
+            }
+            foreach (var item in resimler)
+            {
+                PictureBox p = new PictureBox();
+                p.SizeMode = PictureBoxSizeMode.StretchImage;
+                p.Click += P_Click;
+                p.ImageLocation= item.Attributes["data-full"].Value;
+               
+                tableLayoutPanel1.Controls.Add(p);
+                
+            }
+            labelKirilim.Text = kirilim;
+            labelUrunAdi.Text = urunAd;
+            labelAltBaslik.Text = urunAltBaslık;
+            labelFiyat.Text = urunFiyat;
+           webBrowser1.DocumentText=aciklama[0].InnerHtml;
+            // GetDatas(txtBoxUrl.Text);
+        }//*[@id="tabPanelProDetail"]/div/section[3]/div
+
+        private void P_Click(object sender, EventArgs e)
+        {
+            using (Form form = new Form())
+            {
+
+                form.StartPosition = FormStartPosition.CenterScreen;
+
+                PictureBox pb = new PictureBox();
+
+                pb.Dock = DockStyle.Fill;
+                pb.Image = ((PictureBox)sender).Image;
+                pb.SizeMode = PictureBoxSizeMode.AutoSize;
+                form.Size = pb.Size;
+                form.Controls.Add(pb);
+                form.ShowDialog();
+            }
+            sender.ToString();
+            e.ToString();
+        }
+
+
+      
     }
 }
